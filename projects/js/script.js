143c494b3612d63223f58bb272c6f749238cd087
RassoulEbrahimi <80487962+RassoulEbrahimi@users.noreply.github.com>
2024-07-09 01:29:45 +0200

    Update script.js

--- projects/js/script.js
+++ projects/js/script.js
@@ -51,19 +51,4 @@ window.onscroll = () => {
     }
 }
 
-// Slideshow logic
-let slideIndex = 0;
-showSlides();
 
-function showSlides() {
-    let slides = document.querySelectorAll(".mySlides");
-    slides.forEach(slide => {
-        slide.style.display = "none";
-    });
-    slideIndex++;
-    if (slideIndex > slides.length) {
-        slideIndex = 1;
-    }
-    slides[slideIndex - 1].style.display = "block";
-    setTimeout(showSlides, 2000); // Change image every 2 seconds
-}
